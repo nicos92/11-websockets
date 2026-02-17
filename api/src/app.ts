84ab122703e6659ad0b707d5bb64ff57dcd7aaec
nicokde <nicolassandoval140692@gmail.com>
2026-02-17 14:00:53 -0300

    feat: add connection and disconnection logging for WebSocket clients

--- api/src/app.ts
+++ api/src/app.ts
@@ -13,6 +13,7 @@ async function main() {
 
   wss.on('connection', function connection(ws) {
 
+    // console.log(ws)
     console.log('Client connected')
     ws.on('error', console.error);
 
@@ -25,6 +26,10 @@ async function main() {
     //   ws.send('Hola desde el servidor de nuevo');
 
     // }, 2000);
+
+    ws.on('close', () => {
+      console.log('Client disconected')
+    })
   });
 
   console.log(`http://localhost:3000`)
